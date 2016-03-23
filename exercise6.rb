ffd01c71b5d19f07746624e12833b8e8f8e70c30
CJ <dennischoi@Denniss-MacBook.local>
2016-03-23 14:07:35 -0400

    Complicated code for listing with *...

--- exercise6.rb
+++ exercise6.rb
@@ -1,2 +1,2 @@
 grocery_list = ["Ninja stars", "Nunchucks", "Water-pistol", "Fish", "Mario-Kart Banana peel"]
-puts grocery_list
+puts "* #{grocery_list[0]} \n* #{grocery_list[1]} \n* #{grocery_list[2]} \n* #{grocery_list[3]} \n* #{grocery_list[4]}"
