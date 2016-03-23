d0995cc8f97ce528ada053c2de2a4db724a2c453
CJ <dennischoi@Denniss-MacBook.local>
2016-03-23 13:51:40 -0400

    Call in user_input to method

--- exercise5.rb
+++ exercise5.rb
@@ -3,7 +3,7 @@ user_input = gets.chomp
 user_input = user_input.to_i
 def temperature(f)
   celcius = (f - 32) * 5/9
-  puts "Converted: #{celcius}"
+  puts "Converted from Farenheit to Celcius: #{celcius}"
 end
 
 temperature(user_input)
