aec64ace09a3ecee566c021277fec555f3b48f1b
CJ <dennischoi@Denniss-MacBook.local>
2016-03-23 17:13:49 -0400

    Bonus and Exercise 7

--- exercise7.rb
+++ exercise7.rb
@@ -7,16 +7,33 @@ students = {
 students.each {|k, v| puts "#{k}: has #{v} students"}
 
 students[:cohort4] = 43
-
+# When adding hashes use [] and = or => 'hash rocket'
 puts students.keys
 
 
 students.each do |k, v|
   v2 = v * 1.05
+  # create a variable to make a new value then .round
   puts "#{k} had a 5%... now has #{v2.round} students"
 
 end
 
 students.delete(:cohort2)
-
+# When deleting hashes, use ()
 students.each {|k, v| puts "#{k}: has #{v} students."}
+
+students[:cohort2] = 42
+
+students.each {|k, v| puts "#{k}: #{v}"}
+
+
+ sum = 0
+
+total_students = students.values
+total_students.each {|v| sum += v }
+
+puts "#{sum} students total"
+
+
+s = total_students.inject(0) {|sum, v| sum + v}
+puts s
