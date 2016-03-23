da50a91a468def65bb71cd0a9793a11d05b331ab
CJ <dennischoi@Denniss-MacBook.local>
2016-03-23 13:50:36 -0400

    Method and Formula

--- exercise5.rb
+++ exercise5.rb
@@ -1,2 +1,9 @@
 puts "What is the temperature? in Farenheit..."
 user_input = gets.chomp
+user_input = user_input.to_i
+def temperature(f)
+  celcius = (f - 32) * 5/9
+  puts "Converted: #{celcius}"
+end
+
+temperature(user_input)
