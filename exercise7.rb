8eeef7c322c7fb52ad467b9f8fe949429d791946
CJ <dennischoi@Denniss-MacBook.local>
2016-03-23 16:02:56 -0400

    5% increase in cohorts

--- exercise7.rb
+++ exercise7.rb
@@ -9,3 +9,10 @@ students.each {|k, v| puts "#{k}: has #{v} students"}
 students[:cohort4] = 43
 
 puts students.keys
+
+
+students.each do |k, v|
+  v2 = v * 1.05
+  puts "#{k} had a 5%... now has #{v2.round} students"
+
+end
